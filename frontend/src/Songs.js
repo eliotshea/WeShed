cb2d8fa1977b822c1b7795f56d33a97a3459abbc
Eliot Shea <eliot.shea@gmail.com>
2019-10-15 22:50:44 -0400

    Songs.js Clickable List
    Got working method to display lead sheets along with the tracks. List is clickable, and
    able to switch between songs. The filepath to the lead sheet and backing track url are stored in the state
    of the songList component. Tried to make songList have child components songs, but I couldn't get it to work.
    Kept the name the same because songList is more relevant.

--- frontend/src/Songs.js
+++ frontend/src/Songs.js
@@ -1,57 +1,37 @@
 import React, { Component } from 'react';
+import ReactPlayer from 'react-player';
 import Cookies from 'js-cookie';
 import DI from './config/domain_info';
 import './App.css';
 
 
-//const dir = '../resources/sheets/';
-/*
-const fs = require('fs');
-const songList = ['All The Things You Are', 'Beautiful Love'];
-
-fs.readdir(dir, (err, files) => {
-  files.forEach(file => {
-    songList.push(file);
-  });
-});
-
-
-const ListItem = ({ value }) => (
-  <li>{value}</li>
-);
-
-const List = ({ items }) => (
-  <ul>
-    {
-      items.map((item, i) => <ListItem key={i} value={item} />)
-    }
-  </ul>
-);
-*/
 
 const PREFIX_DIR = './res/sheet_imgs/'
 
-
 //Referenced https://www.robinwieruch.de/react-fetching-data
-class Songs extends Component {
+class songList extends Component {
 	constructor(props) {
 		super(props);
-		
-		var song = { msid: null, Name: null, F_handle: null, Bt_ref: null};
-		
-		this.state = { song_arr: [], };
+		//var song = { msid: null, Name: null, F_handle: null, Bt_ref: null};
+		this.state = {
+			song_arr: [],
+			currentSong: 'stablemates.png' 
+		};
 	}
 	
-	
-	componentDidMount(){
-		
-	fetch(DI.DOMAIN + '/get_songs')
-      .then(response => response.json())
-	  .then(data => this.setState({ song_arr: data }));	
+	componentDidMount(){	
+		fetch(DI.DOMAIN + '/get_songs')
+		.then(response => response.json())
+		.then(data => this.setState({ song_arr: data }));	
+	}
+
+	changeSong(newSong, newTrack) {
+		this.setState({
+			currentSong: newSong,
+			backingTrack: newTrack
+	})
 	}
 	
-	
-  
 	getToken(){
 	  alert(Cookies.get('mytoken'));
 	}
@@ -60,32 +40,30 @@ class Songs extends Component {
 
 render() {
 	
-	const { song_arr } = this.state;
-
+	const {song_arr} = this.state;
+	let player = <p></p>;
+	if(this.state.backingTrack){
+		player = <ReactPlayer url={this.state.backingTrack} controls="true" />
+	}
     return (
       <div className="Songs">
         <h1> Songs page </h1>
 		
-		
 		<div>
-		<img src={ require(PREFIX_DIR + 'stablemates.png') } />
+		<img src={require(`${PREFIX_DIR}${this.state.currentSong}`)} />
+		{player}
 		</div>
 		<ul>
-			{song_arr.map(song_arr =>
-			<li key={song_arr.Msid}>
-				<b>{song_arr.Msid}</b> <b>{song_arr.Name}</b> <b>{song_arr.F_handle}</b> <b>{song_arr.Bt_ref}</b>
-			</li>
+			{song_arr.map(song =>
+				<li key={song.Msid} onClick={() => this.changeSong(song.F_handle, song.Bt_ref)}>
+					<b>{song.Msid}</b> <b>{song.Name}</b> <b>{song.F_handle}</b> <b>{song.Bt_ref}</b>
+				</li>
 			)}
 		</ul>
-		
-		
-		<button onClick={this.getToken}>
-			getToken
-		</button>
-      </div>
+      	</div>
 	  
     );
   }
 }
 
-export default Songs;
+export default songList;
