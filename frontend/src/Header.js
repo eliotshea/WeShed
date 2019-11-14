0f1ae0c3110be9ab17cdb752641eb9de02f806e0
rogelio <roge_kw@outlook.com>
2019-11-14 12:23:37 -0500

    song search page

--- frontend/src/Header.js
+++ frontend/src/Header.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './App.css';
 
+
 class Header extends Component {
 
   render() {
@@ -11,9 +12,16 @@ class Header extends Component {
         <Link to='Register'>Register</Link><br/>
         <Link to='Login'>Login</Link><br/>
         <Link to='Home'>Home</Link><br/>
-		<Link to='Playlists'>Playlists</Link><br/>
+		    <Link to='Playlists'>Playlists</Link><br/>
         <Link to='Songs'>Songs</Link><br/>
         <Link to='Profile'>Profile</Link><br/>
+        <Link to='Search'>
+          <img
+            className="searchIcon"
+            src={require("./searchIcon.png")}
+            style={{width: 30, height: 25}}
+          />
+        </Link><br/>
       </div>
     );
   }
